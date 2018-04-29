31a8e7313c9250888fbea6a61f537cabf723d28d
Quinten Clause <quinten.clause@studioemma.eu>
2018-04-29 15:42:30 +0200

    Add particles from a spritesheet

--- src/index.js
+++ src/index.js
@@ -13,12 +13,39 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+var bg;
+var shards;
+var emitter;
+var timedEvent;
+
 function preload ()
 {
     this.load.image('bg', 'assets/blue.png');
+
+    this.load.spritesheet([{ file: 'assets/shards.png', key: 'shards', config: { frameWidth: 16, frameHeight: 16 } }]);
 }
 
 function create ()
 {
-    var logo = this.add.image(256, 256, 'bg');
+    bg = this.add.image(256, 256, 'bg');
+
+    shards = this.add.particles('shards');
+    emitter = shards.createEmitter({
+        frame: [0, 1, 2, 3],
+        x: 200,
+        y: 300,
+        speed: { min: -800, max: 800 },
+        angle: { min: 0, max: 360 },
+        scale: { start: 2, end: 0 },
+        lifespan: 600,
+        gravityY: 800,
+        frequency: -1,
+        rotate: { min: -540, max: 540 }
+    });
+    timedEvent = this.time.addEvent({ delay: 1000, callback: onEvent, callbackScope: this, loop: true });
+}
+
+function onEvent ()
+{
+    emitter.explode(8, 128 + Math.floor(Math.random() * 256), 128 + Math.floor(Math.random() * 256));
 }
